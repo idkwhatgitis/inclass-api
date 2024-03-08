1ca666d15e2cf6778707c691f187d1c8d46e79fc
liyy0 <yuyanli@bu.edu>
2024-03-08 11:44:17 -0500

    fixed

--- app/src/main/java/com/example/inclass_api/MainActivity.kt
+++ app/src/main/java/com/example/inclass_api/MainActivity.kt
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import com.example.inclass_api.api.OMDb_API
 import com.example.inclass_api.api.OMDb_item
 import com.example.inclass_api.api.movieItem
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import com.squareup.picasso.Picasso
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -28,41 +30,50 @@ class MainActivity : AppCompatActivity() {
             getComments(input.text.toString())
 
         }
-
+        getComments("")
     }
 
     private fun getComments(input:String){
-        val base_url= "http://www.omdbapi.com/"
-        val moshi = Moshi.Builder()
-            .add(KotlinJsonAdapterFactory())
-            .build()
-        val api = Retrofit.Builder()
-            .baseUrl(base_url)
-            .addConverterFactory(MoshiConverterFactory.create(moshi))
-            .build()
-            .create(OMDb_API::class.java)
-            Log.d("aaa",api.toString())
-        api.getMovie("finding nemo","aa982ac0").enqueue(object : Callback<movieItem> {
-            override fun onResponse(
-                call: Call<movieItem>,
-                response: Response<movieItem>
-            ) {
+        if (input == ""){
+            val title = findViewById<TextView>(R.id.title)
+        }else{
+            val base_url= "https://www.omdbapi.com/"
+            val moshi = Moshi.Builder()
+                .add(KotlinJsonAdapterFactory())
+                .build()
+            val api = Retrofit.Builder()
+                .baseUrl(base_url)
+                .addConverterFactory(MoshiConverterFactory.create(moshi))
+                .build()
+                .create(OMDb_API::class.java)
+//                Log.d("aaa",api.toString())
+            api.getMovie(input,"aa982ac0").enqueue(object : Callback<movieItem> {
+                override fun onResponse(
+                    call: Call<movieItem>,
+                    response: Response<movieItem>
+                ) {
 
-                if (response.isSuccessful){
-                    Log.d("???",response.toString())
-                    val news = response.body()!!
-                    Log.d("????",news.toString())
-//                    textview.text = news.articles[0].title
-//                    callback(news)
-
+                    if (response.isSuccessful){
+                        val news = response.body()!!
+//                        Log.d("????",news.toString())
+                        val image = findViewById<ImageView>(R.id.image)
+                        val title = findViewById<TextView>(R.id.title)
+                        val year = findViewById<TextView>(R.id.year)
+                        title.text = news.Title
+                        year.text = news.Year
+                        Picasso.get()
+                            .load(news.Poster)
+                            .into(image)
+                    }
                 }
+
+                override fun onFailure(call: Call<movieItem>, t: Throwable) {
+                    Log.e("???", "onFailure: ${t.message}")
+
+
             }
-
-            override fun onFailure(call: Call<movieItem>, t: Throwable) {
-                TODO("Not yet implemented")
-            }
+            })
+        }}
 
 
-        })
-    }
 }
